4893487979f1a750ae7797fd25e423ea05374050
VictoriusCV <103618949+VictoriusCV@users.noreply.github.com>
2022-05-05 14:38:26 -0400

    Add files via upload

---
+++ EjercicioDate.js
@@ -0,0 +1,21 @@
+time = new Date();
+
+
+/*dia actual
+console.log(fecha.getMinutes());
+//muestra hora en intervalos de 24 horas
+console.log(fecha.getHours());
+//muestra hora en intervalos de 12 horas
+console.log(fecha.getHours() - 12 + "pm");
+//dia de la semana (Domingo=0)
+console.log(fecha.getDay() + " dia");
+console.log(fecha.getMonth() + " Mes");
+console.log(fecha.getFullYear() + " año");
+
+//timestamp 
+console.log(fecha.getTime());
+console.log(fecha);
+*/
+var tiempo = document.getElementById("time").innerHTML;
+document.getElementById("time").innerHTML = time;
+
